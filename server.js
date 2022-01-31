4df2ae5bac2ce7591b74556d20283924cc98d63e
Will <twitchifygamers@gmail.com>
2022-01-31 18:40:43 +0000

    Adjusted console output when running

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true,
 }));
 app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+console.log('Running Grade API server');
